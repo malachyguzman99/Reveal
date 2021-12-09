16317252c8bda777f4894db5146e1e64ff80e704
oTullio <tullioo@carleton.edu>
2021-12-08 22:35:57 -0500

    added ENTER button search functionality

--- Reveal_MVP_1.0/public/js/scripts.js
+++ Reveal_MVP_1.0/public/js/scripts.js
@@ -2,6 +2,20 @@ console.log("js/scripts.js connected to index.html");
 
 document.getElementById("searchBtn").addEventListener("click", returnCompany);
 
+// Get the input field
+var input = document.getElementById("search");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("searchBtn").click();
+  }
+});
+
 function returnCompany() {
   let search = document.getElementById("search").value;
   window.location.href = '/companies/'+search;
@@ -58,3 +72,4 @@ function calculatePreferenceMatchScore()
   score = 4 * score;
   return score;
 }
+
